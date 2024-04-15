dbf70537219a60f9e7aac8d3c427a991ded56fe6
Snigurr <laska135173@gmail.com>
2024-04-15 17:20:24 +0100

    finally?

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,111 +1,130 @@
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+    import iziToast from "izitoast";
+    import "simplelightbox/dist/simple-lightbox.min.css";
+    const addImageButton = document.querySelector(".add_img_btn");
+    const list = document.querySelector("ul");
+    const loader = document.querySelector(".loader");
 
+    let lightbox; 
 
-let lightbox; 
-
-function preloadImages(images) {
-    return new Promise((resolve, reject) => {
-    const imagePromises = images.map(image => {
+    function preloadImages(images) {
         return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.onload = () => resolve();
-            img.onerror = error => reject(error);
-            img.src = image.largeImageURL;
+        const imagePromises = images.map(image => {
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.onload = () => resolve();
+                img.onerror = error => reject(error);
+                img.src = image.largeImageURL;
+            });
         });
+
+        Promise.all(imagePromises)
+            .then(() => resolve())
+            .catch(error => reject(error));
     });
 
-    Promise.all(imagePromises)
-        .then(() => resolve())
-        .catch(error => reject(error));
-});
+    }
 
-}
+    export function renderImages(images, page) {
+        showLoader();
+        preloadImages(images)
+            .then(() => {
+                if (!list) {
+                    throw new Error("List element not found");
+                }
+                list.insertAdjacentHTML("beforeend", createMarkup(images));
+                
+                if (!lightbox) {
+                    lightbox = new SimpleLightbox("ul a", {
+                        captionsData: "alt",
+                        captionDelay: 250,
+                    });
+                } else {
+                    lightbox.refresh();
+                }
 
-export function renderImages(images) {
-    showLoader();
-    preloadImages(images)
-        .then(() => {
-            const list = document.querySelector("ul");
-            
-            if (!list) {
-                throw new Error("List element not found");
+                hideLoader();
+                if(page !== 1){
+                    const height = (list.firstElementChild.getBoundingClientRect().height * 2) + 48;
+                    setTimeout(() => {
+                        window.scrollBy({
+                            top: height,
+                            behavior: 'smooth',
+                        });
+                    }, 300);
+                }
+
+                if(images.length < 15){
+                    hideLoaderButton();
+                    iziToast.warning({
+                        title: "Warning",
+                        message: "There are fewer than 15 images available.",
+                        position: "topRight"
+                    });
+                }
+                
+            })
+            .catch(error => {
+                console.error("Error while preloading images:", error);
+            });
+    }
+
+
+
+    export function clearImages() {
+        hideLoader();
+        hideLoaderButton();
+        return new Promise(resolve => {
+            if (list) {
+                list.innerHTML = "";
             }
-            list.insertAdjacentHTML("beforeend", createMarkup(images));
-            
-            if (!lightbox) {
-                lightbox = new SimpleLightbox("ul a", {
-                    captionsData: "alt",
-                    captionDelay: 250,
-                });
-            } else {
-                lightbox.refresh();
-            }
-
-            hideLoader();
-            
-        })
-        .catch(error => {
-            console.error("Error while preloading images:", error);
+            resolve();
         });
-}
+    }
 
+    function createMarkup(arr) {
+        return arr
+            .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+                <li>
+                    <a href="${largeImageURL}" alt="${tags}">
+                        <img src="${webformatURL}" alt="${tags}" width="360" />
+                        <p class="image-info">
+                            <span>Likes<br> ${likes}</span>
+                            <span>Views<br> ${views}</span>
+                            <span>Comments<br> ${comments}</span>
+                            <span>Downloads<br> ${downloads}</span>
+                        </p>
+                    </a>
+                </li>
+            `)
+            .join("");
+        
+    }
 
-
-export function clearImages() {
-    return new Promise(resolve => {
-        const list = document.querySelector("ul");
-        if (list) {
-            list.innerHTML = "";
+    function showLoader() {
+        if (loader) {
+            loader.style.display = "block";
+            hideLoaderButton();
         }
-        resolve();
-    });
-}
-
-function createMarkup(arr) {
-    return arr
-        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-            <li>
-                <a href="${largeImageURL}" alt="${tags}">
-                    <img src="${webformatURL}" alt="${tags}" width="360" />
-                    <p class="image-info">
-                        <span>Likes<br> ${likes}</span>
-                        <span>Views<br> ${views}</span>
-                        <span>Comments<br> ${comments}</span>
-                        <span>Downloads<br> ${downloads}</span>
-                    </p>
-                </a>
-            </li>
-        `)
-        .join("");
-    
-}
-
-export function showLoader() {
-    const loader = document.querySelector(".loader");
-    if (loader) {
-        loader.style.display = "block";
     }
-}
 
-export function hideLoader() {
-    const loader = document.querySelector(".loader");
-    if (loader) {
-        loader.style.display = "none";
+    function hideLoader() {
+        if (loader) {
+            loader.style.display = "none";
+            addImageButton.style.display = "block";
+        }
     }
-}
 
-export function moveLoaderBelowButton(button) {
-    const loader = document.querySelector(".loader");
-    loader.style.position = "absolute";
-    loader.style.top = button.offsetTop + button.offsetHeight + "px";
-    loader.style.left = button.offsetLeft + "px";
+    function hideLoaderButton(){
+        addImageButton.style.display = "none";
+    }
+
+    export function moveLoaderBelowList() {
+        loader.style.position = "absolute";
+        loader.style.top = list.offsetTop + list.offsetHeight + "px";
 }
 
 export function moveLoaderBelowInput(input) {
-    const loader = document.querySelector(".loader");
-    loader.style.position = "absolute";
-    loader.style.top = input.offsetTop + input.offsetHeight + "px";
-    loader.style.left = input.offsetLeft + "px";
-}
-
+        loader.style.position = "absolute";
+        loader.style.top = input.offsetTop + input.offsetHeight + "px";
+    }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { fetchImages } from "./js/pixabay-api.js";
-import { renderImages, showLoader, clearImages, hideLoader, moveLoaderBelowButton, moveLoaderBelowInput } from "./js/render-functions.js";
+import { renderImages, clearImages, moveLoaderBelowList, moveLoaderBelowInput} from "./js/render-functions.js";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
@@ -11,23 +11,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const addImageButton = document.querySelector(".add_img_btn");
     const API_KEY = "43273771-9fb27172998fac0872066584a";
     
-
     let currentPage = 1;
-    let query = '';
-
-
+    let query;
 
     form.addEventListener("submit", async event => {
         event.preventDefault();
         query = searchInput.value.trim();
-        
         if (query) {
-            
             currentPage = 1; 
             clearImages().then(() => {
                 moveLoaderBelowInput(searchInput);
-                showLoader();
-                fetchAllImages(query);  
+                fetchAllImages(query); 
             });
         } else {
             iziToast.warning({
@@ -39,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     async function fetchAllImages() {
-        const query = searchInput.value.trim();
+        searchInput.value = "";
         const params = new URLSearchParams({
             key: API_KEY,
             q: query,
@@ -53,38 +47,25 @@ document.addEventListener("DOMContentLoaded", function() {
             .then(data => {
                 if (data.hits.length === 0) {
                     clearImages();
-                    showLoader();
                     throw new Error("Sorry, there are no images matching your search query. Please try again!");
                 }
-                renderImages(data.hits, "ul.gallery");
-                
-                if (data.hits.length < 15) {
-                    hideLoadMoreButton();
-                    iziToast.warning({
-                        title: "Warning",
-                        message: "There are fewer than 15 images available.",
-                        position: "topRight"
-                    });
-            }
+                renderImages(data.hits, currentPage);
             })
             .catch(error => {
-                searchInput.value = "";
                 iziToast.error({
                     title: "Error",
                     message: error.message,
                     position: "topRight"
                 });
-            });
+            })
     }
 
     addImageButton.addEventListener("click", () => {
-        showLoader();
-        moveLoaderBelowButton(addImageButton);
+        moveLoaderBelowList();
         onLoadMoreImages();
     });
 
     async function onLoadMoreImages() {
-        showLoader(); 
         currentPage++; 
         try {
             await fetchAllImages();
@@ -94,12 +75,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 message: 'An error occurred while fetching images. Please try again later.',
                 position: 'topRight',
             });
-        } finally {
-            hideLoader();
         }
-    }
-
-    function hideLoadMoreButton() {
-        addImageButton.style.display = "none";
     }
 });
