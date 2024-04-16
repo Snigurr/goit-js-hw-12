cdd1b12f39636d79182a25b8ef329a688257dee1
Snigurr <laska135173@gmail.com>
2024-04-16 22:01:28 +0100

    api fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,12 @@
+
 import axios from "axios";
 
 export async function fetchImages(query) {
-    return axios.get(`https://pixabay.com/api/?${query}`)
-        .then(response => {
-            return response.data;
-        })
-        .catch(error => {
-            console.error("Error while fetching images:", error);
-            throw error;
-        });
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?${query}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error while fetching images:", error);
+        throw error;
+    }
 }
